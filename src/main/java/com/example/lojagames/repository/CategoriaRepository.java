2d5c1aceb20452b7b52599f3941612de117b1bcc
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-05-21 15:03:13 -0300

    :sparkles: Implementação das classes model, repository e controller de Categoria

---
+++ src/main/java/com/example/lojagames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.example.lojagames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.lojagames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+}
